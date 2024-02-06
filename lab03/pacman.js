e0a8792463bbbf0ab8ec1204c761f260e608e98b
KoUrbain <okodj030@uottawa.ca>
2024-02-06 00:38:14 -0500

    2. et 3.

--- lab03/pacman.js
+++ lab03/pacman.js
@@ -1,3 +1,12 @@
+
+
+let pacmanIndex;
+let ghostIndex;
+let fruitIndex;
+let n;
+let input = window.prompt("Entrerla taille du jeu (au moins 3):");
+n = parseInt(input);
+
 function createGame(n) {
     // Vérifiez que n est au moins égal à 3
     if (n < 3) {
@@ -8,15 +17,49 @@ function createGame(n) {
     // Initialisez le tableau avec des pastilles
     let gameArray = Array(n).fill(".");
 
-    // Placez le Pacman, le fantôme et le fruit aux positions appropriées
-    let pacmanIndex = n-1;
-    let ghostIndex = n/2;
-    let fruitIndex = (pacmanIndex + 2) % n;
+    // Placez le Pacman, le fantôme et le fruit a des positions arbitraires
+    pacmanIndex = n-1;
+    ghostIndex = parseInt(n/2);
+    fruitIndex = 1;
 
     gameArray[pacmanIndex] = "C";
-    gameArray[ghostIndex] = "^";
+    gameArray[ghostIndex] = "^.";
     gameArray[fruitIndex] = "@";
 
     return gameArray;
 }
+// Essai 1
+let pacmanGame = createGame(n);
+console.log(pacmanGame);
 
+function moveLeft(game){
+    if(pacmanIndex - 1 >= 0){ // verifie si le prochain emplacement n'est pas hors du tableau
+        game[pacmanIndex] ="."; // remplace par '.' la position que vien de quitter pacman 
+        game[--pacmanIndex] = "C";
+    }
+    else{
+        game[pacmanIndex] ="."; 
+        game[n-1] = "C";// ramene pacman a la fin du tableau
+    }
+    return game;
+}
+function moveRight(game){
+    if(pacmanIndex +1 <n){ // verifie si le prochain emplacement n'est pas hors du tableau
+        game[pacmanIndex] =".";  
+        game[++pacmanIndex] = "C";
+    }
+    else{
+        game[pacmanIndex] ="."
+        game[0] = "C";// ramene pacman au debut du tableau
+    }
+    return game;
+}
+// Essaie 2
+pacmanGame = moveLeft(pacmanGame);
+console.log(pacmanGame);
+// Essaie 3
+pacmanGame = moveRight(pacmanGame);
+console.log(pacmanGame);
+// Essaie 4
+pacmanGame = moveRight(pacmanGame);
+console.log(pacmanGame);
